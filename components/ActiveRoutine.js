ab855b26d70dddc1a4312849b1139e57c2bcb26f
Adam Walton <adamrwalton@gmail.com>
2023-02-11 20:23:56 +0200

    Add AsyncStorage as a semi-permanent storage solution

--- components/ActiveRoutine.js
+++ components/ActiveRoutine.js
@@ -4,10 +4,6 @@ import {Text, View} from 'react-native';
 export default ActiveRoutine = ({route, navigation}) => {
     const {routineName} = route.params;
 
-    useEffect(()=>{
-        console.log(routineName)
-    },[])
-
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>ActiveRoutine is {routineName}</Text>
--- components/RoutinesPage.js
+++ components/RoutinesPage.js
@@ -11,51 +11,101 @@ import RoutineCard from './RoutineCard'
 import SafeViewAndroid from '../helpers/SafeViewAndroid';
 import TitleText from './TitleText';
 import { Ionicons } from '@expo/vector-icons';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 
-const dummyData = {
-  "routines": [
-    {"name": "Morning Routine",
-      "steps":[
-        {"title": "Make bed",
-         "duration": 300},
-         {"title": "Shower and change",
-         "duration": 600},
-         {"title": "Eat breakfast",
-         "duration": 700}
-      ]
-    },
-    {"name": "Clean the kitchen",
-      "steps":[
-      {"title": "Start dishwasher",
-       "duration": 300},
-       {"title": "Wipe counters",
-       "duration": 600},
-       {"title": "Sweep floor",
-       "duration": 700}
-      ]
-    },
-    {"name": "Evening Routine",
-      "steps":[
-      {"title": "Brush teeth",
-       "duration": 150},
-       {"title": "Change",
-       "duration": 100},
-       {"title": "Read until lights out",
-       "duration": 1800}
-      ]
-    }
-  ]
-}
+// const dummyData = {
+//   "routines": [
+//     {"name": "Morning Routine",
+//       "steps":[
+//         {"title": "Make bed",
+//          "duration": 300},
+//          {"title": "Shower and change",
+//          "duration": 600},
+//          {"title": "Eat breakfast",
+//          "duration": 700}
+//       ]
+//     },
+//     {"name": "Clean the kitchen",
+//       "steps":[
+//       {"title": "Start dishwasher",
+//        "duration": 300},
+//        {"title": "Wipe counters",
+//        "duration": 600},
+//        {"title": "Sweep floor",
+//        "duration": 700}
+//       ]
+//     },
+//     {"name": "Evening Routine",
+//       "steps":[
+//       {"title": "Brush teeth",
+//        "duration": 150},
+//        {"title": "Change",
+//        "duration": 100},
+//        {"title": "Read until lights out",
+//        "duration": 1800}
+//       ]
+//     }
+//   ]
+// }
 
 export default RoutinesPage = ({navigation})=>{
 
+  const [routines, setRoutines] = useState([]);
+  const { getItem, setItem } = useAsyncStorage("adhdChecklistRoutines");
+
+  /**
+   * Gets routines saved in local storage and saves them in the component state
+   */
+  const getRoutinesFromStorage = async () => {
+    const routinesFromStorage = await getItem()
+    setRoutines(JSON.parse(routinesFromStorage).routines)
+  }
+
+  /**
+   * Sets routines in local storage.
+   * newRoutines will be converted to JSON string within this function.
+   * It should be an array of routine objects
+   * @param {[]} newRoutines
+   */
+  const setRoutinesInStorage = async (newRoutines) => {
+    await setItem(JSON.stringify(newRoutines))
+    setRoutines(newRoutines)
+  }
+
+  /**
+   * Adds a routine to the routines state and updates it in local storage.
+   * routineToAdd should be a routine object
+   * @param {{}}} routineToAdd 
+   */
+  const addRoutine = (routineToAdd) => {
+    const newRoutines = [...routines, routineToAdd];
+    setRoutinesInStorage(newRoutines)
+  }
+
+  /**
+   * Removes a routine from state and storage
+   * @param {String} routineName 
+   */
+  const removeRoutine = (routineName) => {
+    const newRoutines = routines.filter((routine)=> {
+      if(routine.name === routineToRemove.name){
+        return false
+      }
+    })
+    setRoutinesInStorage(newRoutines)
+  }
+
+  useEffect(() => {
+    getRoutinesFromStorage()
+  },[])
+
   return (
-    <SafeAreaView style={SafeViewAndroid.DroidSafeArea}>
       <View style={styles.container}>
         <TitleText>Your Routines</TitleText>
         <FlatList
           style={styles.list}
-          data={dummyData.routines}
+          data={routines}
           renderItem={(routine, index) => (
             <RoutineCard
               routine={routine.item}
@@ -70,8 +120,6 @@ export default RoutinesPage = ({navigation})=>{
           <Ionicons name="add" size={70} color="#BAFF29" />
         </TouchableOpacity>
       </View>
-      <StatusBar style="light" />
-    </SafeAreaView>
   );
 }
 
